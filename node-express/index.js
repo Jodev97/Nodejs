98cb8d55f236d7f955eae2c31cb4dfc8ea2d0de6
Jairo Andres <jairoquinterodev@gmail.com>
2021-07-19 13:59:11 -0500

    Express Serve Static Files

--- node-express/index.js
+++ node-express/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express()
 
 app.set('port',process.env.PORT || 3000)
 
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname+'/public'))
 app.use((req,res,next)=>{
     
     res.statusCode=200
